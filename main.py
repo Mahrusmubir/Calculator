19c00645dd5981679020732d3f67357626a20af5
Mahrusmubir <95760958+Mahrusmubir@users.noreply.github.com>
2022-08-15 12:48:29 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "!")
+print("Good to hear your " + age + " I am 13")
+
+print("Entering calculator")
+
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2)
+
+print(result)
+
+print("Entering decimal calculator")
+
+num1 = input("Enter number:")
+num2 = input("Enter another number:")
+result = float(num1) + float(num2)
+
+print(result)
